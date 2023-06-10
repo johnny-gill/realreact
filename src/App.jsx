b8c9d9a813fa785cb956ba6e7fb08f9d28a168f2
johnny-gill <uint777@gmail.com>
2023-06-10 16:55:18 +0900

    useEffect 뭔가 신기한듯

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from 'react';
 import { ColoredMessage } from './components/ColoredMessage';
 
 export const App = () => {
-  const onClickButton = () => {
-    alert();
+  console.log('Rendering App');
+
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+
+  useEffect(() => {
+    console.log('mount 및 num2이 바뀔때만~');
+  }, [num2]);
+
+  const onClickButton1 = () => {
+    setNum1((prev) => prev + 1);
+  };
+
+  const onClickButton2 = () => {
+    setNum2((prev) => prev + 1);
   };
 
   return (
@@ -10,7 +24,10 @@ export const App = () => {
       <h1 style={{ color: 'red' }}>안녕</h1>
       <ColoredMessage color="blue">사랑해요?</ColoredMessage>
       <ColoredMessage color="pink">사랑해요!</ColoredMessage>
-      <button onClick={onClickButton}>버튼</button>
+      <button onClick={onClickButton1}>버튼1</button>
+      <p>{num1}</p>
+      <button onClick={onClickButton2}>버튼2</button>
+      <p>{num2}</p>
     </>
   );
 };
